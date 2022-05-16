ef071cdf15a076a4c7dfbc520b597e99272cdfd9
Subham Sabat <subhamsabat343@gmail.com>
2022-05-16 22:37:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function changeBG(color){
+    document.body.style.backgroundColor=color;
+    let txt=document.getElementsByClassName('text')
+    if(color=="black")
+    {
+        for(let elm of txt)
+        {
+            elm.style.color="white";
+        }
+
+    }
+    else
+    {
+        for(let elm of txt)
+        {
+            elm.style.color="black";
+        }
+
+    }
+
+    
+
+}
